d99cc80654f1118c7aa69f28c3f7314d65351546
HadiKa47 <Hade.Kary24@gmail.com>
2024-06-16 12:22:58 +0300

    Added Correct URL

--- app/actions.ts
+++ app/actions.ts
@@ -173,8 +173,14 @@ export async function BuyProduct(formData: FormData) {
         destination: data?.User?.connectedAccountId as string,
       },
     },
-    success_url: "http://localhost:3000/payment/success",
-    cancel_url: "http://localhost:3000/payment/cancel",
+    success_url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/payment/success"
+        : "https://digital-marketplace-orcin.vercel.app/payment/success",
+    cancel_url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/payment/cancel"
+        : "https://digital-marketplace-orcin.vercel.app/payment/cancel",
   });
 
   return redirect(session.url as string);
@@ -200,8 +206,14 @@ export async function CreateStripeAccoutnLink() {
 
   const accountLink = await stripe.accountLinks.create({
     account: data?.connectedAccountId as string,
-    refresh_url: `http://localhost:3000/billing`,
-    return_url: `http://localhost:3000/return/${data?.connectedAccountId}`,
+    refresh_url:
+      process.env.NODE_ENV === "development"
+        ? `http://localhost:3000/billing`
+        : `https://https://digital-marketplace-orcin.vercel.app/billing`,
+    return_url:
+      process.env.NODE_ENV === "development"
+        ? `http://localhost:3000/return/${data?.connectedAccountId}`
+        : `https://https://digital-marketplace-orcin.vercel.app/return/${data?.connectedAccountId}`,
     type: "account_onboarding",
   });
 
